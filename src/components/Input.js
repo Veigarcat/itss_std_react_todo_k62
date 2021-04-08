f2fff3b43d9207f771f6b495c9f5caa38a94005b
Veigarcat <s2kimphongs2@gmail.com>
2021-04-08 18:29:15 +0000

    Add new todo

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,10 +4,29 @@ import React, { useState } from 'react';
 　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
 　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
 */
-function Input(  ) {
+
+function Input({ onAddItem }) {
+
+  const [newTodo, setNewTodo] = useState('')
+
+  const enterHandler = (e) => {
+    if (e.keyCode === 13) {
+      setNewTodo(e.target.value)
+      onAddItem(newTodo)
+      setNewTodo('')
+    }
+  }
 
   return (
     <div className="panel-block">
+      <input
+        className="input"
+        type="text"
+        placeholder="Enter to add"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        onKeyDown={enterHandler}
+      />
     </div>
   );
 }
